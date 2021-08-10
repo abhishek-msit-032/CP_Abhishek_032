92ff658b187b890bb91cbcb9042cc7f6d2778601
abhishek-msit-032 <abhishekbejavada@msitprogram.net>
2021-08-10 10:35:27 +0530

    completed

--- 07-isfactor-Python/isfactor.py
+++ 07-isfactor-Python/isfactor.py
@@ -6,4 +6,14 @@
 
 
 def fun_isfactor(f, n):
-	return False # replace with your solution
+	f=abs(f)
+	n=abs(n)
+	if f==0 and n>0:
+		return False
+	elif f%2==0 and n%2==0:
+		return True
+	else:
+		return False
+	
+
+

e626325a4308b5d3858cf0a131bc76f4bd229107
abhishek-msit-032 <abhishekbejavada@msitprogram.net>
2021-08-13 17:47:19 +0530

    completed

--- 03-kth_occurrences-Python/kth_occurrences.py
+++ 03-kth_occurrences-Python/kth_occurrences.py
@@ -4,7 +4,28 @@
 
 
 
-def fun_kth_occurrences(s, n):
-	return 'a'
-
-
+def fun_kth_occurrences(n, k):
+    n=list(n)
+    n.sort()
+    
+    dic={}
+    for i in n:
+        if i  in dic:
+            dic[i]=dic[i]+1
+        else:
+            dic[i]=1
+    
+    res=[]
+    max=0
+    ans=dict(sorted(dic.items(), key=lambda item: item[1]))
+    print(ans)
+    # for i,j in sorted(ans.items()):
+    #   if j>=max:    
+    #     res.append(i)
+    #     #print(res)
+    # return res
+    li=[]
+    max=0
+    for i,j in ans.items():
+        li.append([i,j])
+    return li[-k][0]

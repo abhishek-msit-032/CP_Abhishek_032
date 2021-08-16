126cb93a265cb059282a4d17c0edd98ba0af2962
abhishek-msit-032 <abhishekbejavada@msitprogram.net>
2021-08-16 21:12:48 +0530

    completed

--- 11-ismostlymagicsquare-Python/ismostlymagicsquare.py
+++ 11-ismostlymagicsquare-Python/ismostlymagicsquare.py
@@ -1,9 +1,9 @@
 # isMostlyMagicSquare(a) [15 pts]
-# Write the function isMostlyMagicSquare(a) that takes an 2d list of numbers, which you may assume is an NxN square 
+# Write the function isMostlyMagicSquare(a) that takes an 2d list of numbers, which you may assume is an NxN square
 # with N>0, and returns True if it is "mostly magic" and False otherwise, where a square is "mostly magic" if:
 # Each row, each column, and each of the 2 diagonals each sum to the same total.
-# A completely magic square has additional restrictions (such as not allowing duplicates, and only allowing numbers 
-# from 1 to N2), which we are not enforcing here, but which you can read about here. Note: any magic square is also 
+# A completely magic square has additional restrictions (such as not allowing duplicates, and only allowing numbers
+# from 1 to N2), which we are not enforcing here, but which you can read about here. Note: any magic square is also
 # a "mostly magic" square, including this sample magic square:
 # Read for more: https://en.wikipedia.org/wiki/Magic_square
 # Here is another mostly-magic square:
@@ -14,5 +14,21 @@
 # Each row and each column add to 3, but one diagonal adds to 2 and the other to 4.
 
 def ismostlymagicsquare(a):
-	# Your code goes here
-	pass
+    # Your code goes here
+    r = len(a)
+    c = len(a[0])
+
+    if(len(a) == len(a[0])):
+        for i in range(0, r):
+            sr = 0
+            for j in range(0, c):
+                sr = sr + a[i][j]
+
+        for i in range(0, r):
+            sc = 0
+            for j in range(0, c):
+                sc = sc + a[j][i]
+
+        if(sr == sc):
+            return True
+        return False
